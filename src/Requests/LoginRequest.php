0624153c5877f12ca674679c7b9819e46a8d3467
Jose Fernandez <self@josefernandez.me>
2017-04-17 15:35:22 -0400

    Applied new style to LoginRequest

--- src/Requests/LoginRequest.php
+++ src/Requests/LoginRequest.php
@@ -15,12 +15,16 @@ use JMS\Serializer\Annotation\XmlRoot;
 class LoginRequest extends Request
 {
     /**
+     * @var int
+     *
      * @SerializedName("IAID")
      * @Type("integer")
      */
     protected $appId;
 
     /**
+     * @var string
+     *
      * @SerializedName("IAName")
      * @Type("string")
      * @XmlElement(cdata=false)
@@ -28,6 +32,8 @@ class LoginRequest extends Request
     protected $appName;
 
     /**
+     * @var string
+     *
      * @SerializedName("IADescription")
      * @Type("string")
      * @XmlElement(cdata=false)
@@ -35,6 +41,8 @@ class LoginRequest extends Request
     protected $appDescription;
 
     /**
+     * @var string
+     *
      * @SerializedName("UserName")
      * @Type("string")
      * @XmlElement(cdata=false)
@@ -42,59 +50,131 @@ class LoginRequest extends Request
     protected $username;
 
     /**
+     * @var string
+     *
      * @SerializedName("UserPassword")
      * @Type("string")
      * @XmlElement(cdata=false)
      */
     protected $password;
 
+    /**
+     * Gets the value of appId.
+     *
+     * @return int
+     */
     public function getAppId()
     {
         return $this->appId;
     }
 
+    /**
+     * Sets the value of appId.
+     *
+     * @param int $appId the app id
+     *
+     * @return self
+     */
     public function setAppId($appId)
     {
         $this->appId = $appId;
+
+        return $this;
     }
 
+    /**
+     * Gets the value of appName.
+     *
+     * @return string
+     */
     public function getAppName()
     {
         return $this->appName;
     }
 
+    /**
+     * Sets the value of appName.
+     *
+     * @param string $appName the app name
+     *
+     * @return self
+     */
     public function setAppName($appName)
     {
         $this->appName = $appName;
+
+        return $this;
     }
 
+    /**
+     * Gets the value of appDescription.
+     *
+     * @return string
+     */
     public function getAppDescription()
     {
         return $this->appDescription;
     }
 
+    /**
+     * Sets the value of appDescription.
+     *
+     * @param string $appDescription the app description
+     *
+     * @return self
+     */
     public function setAppDescription($appDescription)
     {
         $this->appDescription = $appDescription;
+
+        return $this;
     }
 
+    /**
+     * Gets the value of username.
+     *
+     * @return string
+     */
     public function getUsername()
     {
         return $this->username;
     }
 
+    /**
+     * Sets the value of username.
+     *
+     * @param string $username the username
+     *
+     * @return self
+     */
     public function setUsername($username)
     {
         $this->username = $username;
+
+        return $this;
     }
 
+    /**
+     * Gets the value of password.
+     *
+     * @return string
+     */
     public function getPassword()
     {
         return base64_encode(md5($this->password, true));
     }
 
+    /**
+     * Sets the value of password.
+     *
+     * @param string $password the password
+     *
+     * @return self
+     */
     public function setPassword($password)
     {
         $this->password = $password;
+
+        return $this;
     }
 } 
